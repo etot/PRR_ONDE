d3368dabe8f50f6db103881c3f0b361cff54e8b6
Grégoire Etot <gregoire.etot@gmail.com>
2023-06-16 17:02:06 +0200

    desactivation verif maj pour test

--- 01_verifier_maj.R
+++ 01_verifier_maj.R
@@ -59,6 +59,8 @@ if (!file.exists("data/onde_data/onde.csv") |
   }
 }
 
+to_update <- TRUE
+
 save(to_update, date_jour, date_jour_heure, file = "data/onde_data/to_update.rda")
   
 print(switch(as.character(to_update), `TRUE` = "Mise-à-jour requise", `FALSE` = "Pas de mise-à-jour requise"))
