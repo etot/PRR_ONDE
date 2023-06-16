c8195479cf4034d9aa1e4d7d997e5640eed61818
Grégoire Etot <gregoire.etot@gmail.com>
2023-06-16 16:27:28 +0200

    2 depts seulement + titre test

--- _config.R
+++ _config.R
@@ -1,4 +1,4 @@
 ## configuration
-conf_titre <- "[Prototype] Suivi des étiages (ONDE) en Normandie"
-conf_auteur <- "OFB - Direction régionale Normandie"
-conf_dep <- c("14", "27", "50", "61", "76")
+conf_titre <- "[Prototype] test"
+conf_auteur <- "OFB - Direction test"
+conf_dep <- c("14", "27")
